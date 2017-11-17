49a8be502bc17bafb16e9ec39adb8362e067d8fe
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2017-11-18 12:40:02 +1300

    Massive Overhaul of Python script

--- light.py
+++ light.py
@@ -1,122 +1,146 @@
 import subprocess
 import urllib
+import os
+import pickle
 from datetime import datetime
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from flask import Flask
 
-startTime = 12;
-endTime = 17;
+app = Flask(__name__)
 
-app = Flask(__name__, static_url_path='/static')  
+path = os.path.dirname(os.path.realpath(__file__))
+
+startTime = 12
+endTime = 17
+
+power = ("Off", "On")
+colors = {
+    "White": "#FFFFFF",
+    "Red": "#ED1515",
+    "Dark Orange": "#F75B07",
+    "Orange": "#F79307",
+    "Amber": "#F7D307",
+    "Yellow": "#F2EE0A",
+    "Green": "#2AF72A",
+    "Light Green": "#00FA7D",
+    "Light Blue": "#00DDFA",
+    "Blue": "#0059FF",
+    "Indigo": "#8400FF",
+    "Purple": "#BF00FF",
+    "Pink": "#F200FF"
+}
+minBrightness = 0
+maxBrightness = 5
+
+
+class State:
+    def __init__(self, power, brightness, color):
+        self.power = power
+        self.brightness = brightness
+        self.color = color
+
+
+def url(string):
+    return string.lower().replace(" ", "_")
+
+
+def read():
+    return pickle.load(open(os.path.dirname(os.path.realpath(__file__)) + '/state.data', 'rb'))
+
+
+def write(power=None, brightness=None, color=None):
+    new = read()
+    if power is not None:
+        new.power = power
+    if brightness is not None:
+        new.brightness = brightness
+    if color is not None:
+        new.color = color
+    pickle.dump(new, open(os.path.dirname(os.path.realpath(__file__)) + '/state.data', 'wb'))
+
+
+@app.route("/hour")
+def hour():
+    return "%s" % datetime.now().hour
+
+
+@app.route("/power")
+def getpower():
+    return "%s" % read().power
+
+
+@app.route("/brightness")
+def brightness():
+    return "%s" % read().brightness
+
+
+@app.route("/color")
+def color():
+    state = read()
+    if state.power == power[0]:
+        return power[0]
+    else:
+        return "%s" % read().color
+
+
+@app.route("/hex")
+def hex():
+    state = read()
+    if state.power == power[0]:
+        return "#000000"
+    else:
+        return "%s" % colors.get(state.color)
+
+
+@app.route("/on")
+def on():
+    write(power=power[1])
+    return 'Success', 200
+
+
+@app.route("/off")
+def off():
+    write(power=power[0])
+    return 'Success', 200
+
+
+@app.route("/brighter")
+def brighter():
+    state = read()
+    if state.brightness < maxBrightness:
+        brightness = state.brightness + 1
+        write(brightness=brightness)
+    return 'Success', 200
+
+
+@app.route("/dim")
+def dim():
+    state = read()
+    if state.brightness > minBrightness:
+        brightness = state.brightness - 1
+        write(brightness=brightness)
+    return 'Success', 200
+
+
+@app.route("/darkest")
+def darkest():
+    write(brightness=minBrightness)
+    return 'Success', 200
+
+
+@app.route("/brightest")
+def brightest():
+    write(brightness=maxBrightness)
+    return 'Success', 200
+
+
+@app.route("/setcolor/<color>")
+def setcolor(color=None):
+    for key, value in colors.iteritems():
+        if color == key or color == url(key):
+            write(color=key)
+            return 'Success', 200
+    return 'Invalid Color', 400
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
-
-colorRef=["White","Red","Dark_Orange","Orange","Amber","Yellow","Green","Light_Green","Light_Blue","Blue","Indigo","Purple","Pink"]
-colorHex=["#FFFFFF","#ED1515","#F75B07","#F79307","#F7D307","#F2EE0A","#2AF72A","#00FA7D","#00DDFA","#0059FF","#8400FF","#BF00FF","#F200FF"]
-colors=["white","red","darkorange","orange","amber","yellow","green","lightgreen","lightblue","blue","indigo","purple","pink"]
-colorCommand=["WHITE","RED","DARKORANGE","ORANGE","AMBER","YELLOW","GREEN","LIGHTGREEN","LIGHTBLUE","BLUE","INDIGO","PURPLE","PINK"]
-
-def get(file):
-	with open('/var/www/markmetcalfe/light/'+file+'.state', 'r') as f:
-		data = f.read()
-		f.close()
-		return data
-		
-def write(file, str):
-	with open('/var/www/markmetcalfe/light/'+file+'.state', 'w') as f:
-		f.truncate()	
-		f.write(str)
-		f.close()
-
-@app.route("/")
-def mainpage():
-	with open('/var/www/markmetcalfe/light/index.html', 'r') as f:
-		data = f.read()
-		f.close()
-		return data	
-
-@app.route("/cmd/<state>")                                                          
-def output(state=None):
-	if state == 'gethour':
-		return str(datetime.now().hour)
-	elif state == 'getpower':
-		return get('power')
-	elif state == 'getbrightness':		
-		return get('brightness')
-	elif state == 'getcolor':		
-		return get('color')
-	elif state == 'gethex':
-		with open('/var/www/markmetcalfe/light/color.state', 'r') as c:
-			color = c.read()
-			c.close()
-		if get('power')=='Off':
-			return '#000000'
-		else:
-			i = 0
-			done = False
-			for color in colorRef:
-				if color == colorHex[i]:
-					done = True
-					return colorHex[i]
-				i+=1
-			if done == False:
-				print "Failed"
-				return "Failed"
-	elif state == 'toggle':
-		if get("power") == 'On':
-			write("power","Off")
-			return 'Success'
-		elif get("power") == 'Off':
-			write("power","On")
-			return 'Success'
-	elif state == 'on':
-		write("power","On")		
-		return 'Success'  
-	elif state == 'off':
-		write("power","Off")
-		return 'Success'  
-	elif state == 'brighter':
-		number = int(get("brightness"))
-		if number < 5:	
-			number += 1
-			write("brightness",str(number))
-		return 'Success'  
-	elif state == 'dim':
-		number = int(get("brightness"))
-		if number > 1:	
-			number -= 1
-			write("brightness",str(number))
-		return 'Success'  
-	elif state == 'darkest':
-		write("brightness","1")
-		return 'Success'  
-	elif state == 'brightest':
-		write("brightness","5")
-		return 'Success' 
-	elif state == 'flashfast':
-		write("color","Flash_Fast")
-		return 'Success'  
-	elif state == 'flashslow':
-		write("color","Flash_Slow")
-		return 'Success'  
-	elif state == 'smoothfast':	
-		write("color","Smooth_Fast")
-		return 'Success'  
-	elif state == 'smoothslow':
-		write("color","Smooth_Slow")
-		return 'Success'  		
-	else:
-		done = False
-		i = 0
-		for color in colors:
-			if state.lower() == color.lower():
-				with open('/var/www/markmetcalfe/light/color.state', 'w') as c:
-					c.truncate()	
-					c.write(colorRef[i])
-					c.close()
-				done = True
-				return 'Success'
-			i+= 1
-		if done == False:
-			print 'Failed'
-			return '<html><head><title>Failed</title></head><body><font style="font-family: Arial;"><b>Not A Command</b></font></body></html>'
+    app.run(debug=True)
