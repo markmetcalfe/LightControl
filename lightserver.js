c8d6ef6935083589c572ad9b9e874240a4187b7c
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2018-07-14 15:35:56 +1200

    Abandon gpio functionality

--- lightserver.js
+++ lightserver.js
@@ -6,26 +6,14 @@ const config = require('./config.json');
 const lirc = require('node-lirc');
 app.use(bodyParser.json());
 lirc.init();
-gpio.setup(config.gpio_pin, gpio.DIR_HIGH);
 
 app.post('*', (req, res) => {
-    if(req.body.command){
-        if(config.commands.hasOwnProperty(req.body.command)){
-            lirc.send(config.remote, config.commands[req.body.command]);
-            res.sendStatus(200);
-        } else {
-            res.json("Invalid Command").sendStatus(400);
-        }
-    }
-    if(req.body.users){
-        gpio.write(config.gpio_pin, (req.body.users)?true:false);
+    if(config.commands.hasOwnProperty(req.body.command)){
+        lirc.send(config.remote, config.commands[req.body.command]);
         res.sendStatus(200);
+    } else {
+        res.json("Invalid Command").sendStatus(400);
     }
-    res.sendStatus(400);
-});
-
-app.get('*', (req, res) => {
-    res.sendStatus(200);
 });
 
 app.listen(5001, () => console.log("Server running on port 5001"));
